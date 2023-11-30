da89387a7ce07160d011e823a2a56bec4a1c5e78
Amit Akoka <amitak@bynet.co.il>
2023-11-30 12:31:55 +0200

    adding railway database url

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dbURL: `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@collabmusiccluster.8owqxyn.mongodb.net/?retryWrites=true&w=majority`,
+  dbURL: process.env.RAILWAY_DATABASE_URL,
 };
