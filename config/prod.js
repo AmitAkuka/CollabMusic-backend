21945703964ebc7f66669af3c0e6b6832cf67d03
Amit Akoka <amitak@bynet.co.il>
2023-11-30 10:23:27 +0200

    Added atlas user and pass to env

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-  dbURL:
-    "mongodb+srv://collabmusicnoreply:<password>@collabmusiccluster.8owqxyn.mongodb.net/?retryWrites=true&w=majority",
+  dbURL: `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@collabmusiccluster.8owqxyn.mongodb.net/?retryWrites=true&w=majority`,
 };
